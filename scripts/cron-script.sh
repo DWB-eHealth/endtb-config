7a5594bfb3cc6a3d163ec1c94e8dd7328673678e
Alekhya <alekhyayalla23@gmail.com>
2019-07-15 15:40:44 +0530

    Revert External cohort
    This reverts commit cb052a2629672c744cfd3c2c0c262f6b5c47ab82.

--- scripts/cron-script.sh
+++
@@ -1,9 +0,0 @@
-sudo su -s /bin/bash bahmni << block
-echo "Process Started...";
-/usr/bin/bahmni-batch belongs_to_external_cohort="${1-E1 : endTB external cohort}" job_name=${1-E1 : endTB external cohort};
-/usr/bin/bahmni-batch belongs_to_external_cohort="${2-E2}" job_name=${2-E2};
-/usr/bin/bahmni-batch belongs_to_external_cohort="${3-E3}" job_name=${3-E3};
-/usr/bin/bahmni-batch;
-echo "Finished Exporting COHORTS";
-exit;
-block
